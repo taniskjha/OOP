4facaaff39d07a71219d4e16ce21929b58648c8f
tanisk jha <jhatanishqq@outlook.com>
2020-02-13 20:00:02 +0530

    last commit

---
+++ promise2.js
@@ -0,0 +1,40 @@
+// creating mom job Promise 
+
+const newJob = (hardwork) => {
+    return new Promise((resolve,reject) => {
+        if(!hardwork) {
+            reject(`try again`)
+        } else {
+            resolve(`you got new job because of hardwork of ${hardwork}`)
+        }
+    })
+}
+
+// consuming Job promise 
+newJob('24h').then((fullfilled) => {
+    console.log(fullfilled);
+})
+.catch((err) => console.log(err));
+
+
+// Promise Chaining 
+
+// lets say you Promised your sister for car  once you get the job 
+
+const giftCarAfterJob = Job => {
+  return new Promise((resolve,reject) => {
+    resolve(`Hey sis i got a job now you can get new car`);
+    
+
+  })}
+
+  newJob('24h').then(giftCarAfterJob)
+               .then(fullfilled => {
+                   console.log(fullfilled)
+               } )
+                
+               .catch(err => console.log(err))
+       
+
+
+
